f2bf1e0000a0667c46478c67aa5719fcc2f79480
the-flash <flash@remainsilent.net>
2021-02-18 02:07:27 +0000

    msm8937-common: Split this commit up later

--- msm8937.mk
+++ msm8937.mk
@@ -22,7 +22,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 # Properties
 -include $(LOCAL_PATH)/properties.mk
