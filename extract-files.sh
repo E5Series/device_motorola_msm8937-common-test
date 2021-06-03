ed738ad7322af6033ba20afe078422b426a72d08
Jarl-Penguin <jarlpenguin@outlook.com>
2021-06-03 07:33:33 +0300

    Revert "msm8937-common: Kill legacy keystore"
    This reverts commit a76efa64d78749d509a56f8cdac58d0adf8a44c2.
    
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I7f617fd6f62691fddc4ca4e0f3f57a33c707ead7

--- extract-files.sh
+++ extract-files.sh
@@ -5,68 +5,45 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 set -e
-
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
 ANDROID_ROOT="${MY_DIR}/../../.."
-
 HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
 source "${HELPER}"
-
 function blob_fixup() {
     case "${1}" in
         lib64/libwfdnative.so)
             "${PATCHELF}" --remove-needed android.hidl.base@1.0.so "${2}"
             ;;
-
         # memset shim
         vendor/bin/charge_only_mode)
             for LIBMEMSET_SHIM in $(grep -L "libmemset_shim.so" "${2}"); do
                 "${PATCHELF}" --add-needed "libmemset_shim.so" "${LIBMEMSET_SHIM}"
             done
             ;;
-
         vendor/lib/hw/activity_recognition.msm8937.so | vendor/lib64/hw/activity_recognition.msm8937.so)
             "${PATCHELF}" --set-soname activity_recognition.msm8937.so "${2}"
             ;;
-
-        vendor/lib/hw/camera.msm8937.so)
-            "${PATCHELF}" --set-soname camera.msm8937.so "${2}"
-            ;;
-
         vendor/lib64/hw/gatekeeper.msm8937.so)
             "${PATCHELF}" --set-soname gatekeeper.msm8937.so "${2}"
             ;;
 
-        vendor/lib/libactuator_dw9767_truly.so)
-            "${PATCHELF}" --set-soname libactuator_dw9767_truly.so "${2}"
-            ;;
-
-        # Fix camera recording
-        vendor/lib/libmmcamera2_pproc_modules.so)
-            sed -i "s/ro.product.manufacturer/ro.product.nopefacturer/" "${2}"
-            ;;
-
-        vendor/lib/libmmcamera2_sensor_modules.so)
-            sed -i 's|msm8953_mot_deen_camera.xml|msm8937_mot_camera_conf.xml|g' "${2}"
+        vendor/lib64/hw/keystore.msm8937.so)
+            "${PATCHELF}" --set-soname keystore.msm8937.so "${2}"
             ;;
 
         vendor/lib/libmot_gpu_mapper.so | vendor/lib/libmmcamera_vstab_module.so)
             sed -i "s/libgui/libwui/" "${2}"
             ;;
-
         vendor/lib64/libmdmcutback.so)
             sed -i "s|libqsap_sdk.so|libqsapshim.so|g" "${2}"
             ;;
-
         vendor/lib64/libril-qc-qmi-1.so)
             for LIBCUTILS_SHIM in $(grep -L "libcutils_shim.so" "${2}"); do
                 "${PATCHELF}" --add-needed "libcutils_shim.so" "${LIBCUTILS_SHIM}"
@@ -74,16 +51,13 @@ function blob_fixup() {
             ;;
     esac
 }
-
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
-
 ONLY_BOARD_COMMON=
 ONLY_DEVICE_COMMON=
 ONLY_TARGET=
 KANG=
 SECTION=
-
 while [ "${#}" -gt 0 ]; do
     case "${1}" in
         --only-board-common )
@@ -111,32 +85,24 @@ while [ "${#}" -gt 0 ]; do
     esac
     shift
 done
-
 if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
-
 if [ -z "${ONLY_TARGET}" ] && [ -z "${ONLY_DEVICE_COMMON}" ]; then
     # Initialize the helper
     setup_vendor "${BOARD_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
-
     extract "${MY_DIR}/proprietary-files.txt" "${SRC}" ${KANG} --section "${SECTION}"
 fi
-
 if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_TARGET}" ] && [ -s "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" ];then
     # Reinitialize the helper for device common
     source "${MY_DIR}/../${DEVICE_COMMON}/extract-files.sh"
     setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
     extract "${MY_DIR}/../${DEVICE_COMMON}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
-
 if [ -z "${ONLY_BOARD_COMMON}" ] && [ -z "${ONLY_DEVICE_COMMON}" ] && [ -s "${MY_DIR}/../${DEVICE}/proprietary-files.txt" ]; then
     # Reinitialize the helper for device
     source "${MY_DIR}/../${DEVICE}/extract-files.sh"
     setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
     extract "${MY_DIR}/../${DEVICE}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 fi
-
 "${MY_DIR}/setup-makefiles.sh"
