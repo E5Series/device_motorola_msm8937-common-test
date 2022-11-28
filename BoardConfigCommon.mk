moto-msm89xx/android_device_motorola_msm8937-common

msm8937-common: Don't let builds complete without vendor tree
* There is zero reason to ever build without blobs. We've even seen
  this situation with official builds from our servers. It's always
  better for a build to fail than it is for it to produce something
  that has no chance at working.

Change-Id: I4968795670c91f691e9ecdc0e4af62e16ba3a93a

BoardConfigCommon.mk
@@ -168,4 +168,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/motorola/msm8937-common/BoardConfigCommonVendor.mk
+include vendor/motorola/msm8937-common/BoardConfigCommonVendor.mk

E5Series/device_motorola_msm8937-common-test

BoardConfigCommon.mk
@@ -177,4 +177,4 @@ WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
--include vendor/motorola/msm8937-common/BoardConfigCommonVendor.mk
+include vendor/motorola/msm8937-common/BoardConfigVendor.mk